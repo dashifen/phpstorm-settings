a59f1a0d72fe20f08bf0846dfa7ed86789f4ed13
Administrator <Administrator@130.126.152.207>
2017-01-10 16:39:45 -0500

    PS-2016.3.2 <Administrator@engrit-kees Create PHP Class.php, IntelliLang.xml, ide.general.xml, git.xml, Default.xml, project.default.xml, diff.xml, laf.xml, other.xml, ui.lnf.xml, Default _1_.xml, web-browsers.xml, php.xml, keymap.xml, editor.codeinsight.xml, code.style.schemes.xml, GitHub-Remixed.icls, colors.scheme.xml, databaseSettings.xml, Dashifen.icls, databaseDrivers.xml, editor.xml, debugger.xml, PHP File.php

---
+++ fileTemplates/internal/PHP Class.php
@@ -0,0 +1,10 @@
+<?php
+class ${FILE_NAME} extends dbo {
+
+
+    public function form() { }
+    public function validate() {
+        $this->errors = array();
+        return $this->errors;
+    }
+}
